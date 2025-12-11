617254cbd8d38732d2ebbd2c7843d355f8517d45
znj0103 <znj0103@163.com>
2025-12-11 19:21:08 +0800

    完成09

--- untitled1.py
+++ untitled1.py
@@ -284,40 +284,35 @@ Created on Tue Mar 25 13:24:46 2025
 # print(scores)
 
 #列表的应用
-#例4 双色球
-import random
-n = int(input('几注：'))
-red_balls = [i for i in range(1,34)]
-blue_balls = [i for i in range(1,17)]
-ball = []
+# #例4 双色球
+# import random
 
+# from rich.console import Console
+# from rich.table import Table
 
-from rich.console import Console
-from rich.table import Table
+# #创建工作台
+# console = Console()
 
-# 创建控制台
-console = Console()
+# red_balls = [i for i in range(1,34)]
+# blue_balls = [i for i in range(1,17)]
+# n = int(input('下几注：'))
 
-n = int(input('生成几注号码: '))
-red_balls = [i for i in range(1, 34)]
-blue_balls = [i for i in range(1, 17)]
+# #表头
+# table = Table(show_header= True)
+# for col_name in ('序号','红球','蓝球'):
+#     table.add_column(col_name,justify='center')
 
-# 创建表格并添加表头
-table = Table(show_header=True)
-for col_name in ('序号', '红球', '蓝球'):
-    table.add_column(col_name, justify='center')
+# for i in range(n):
+#     selected_ball = random.sample(red_balls,6)
+#     selected_ball.sort()
+#     blue_ball = random.choice(blue_balls)
+#     #行
+#     table.add_row(
+#         str(i+1),
+#         f'[red]{" ".join([f"{ball:0>2d}" for ball in selected_ball])}[/red]',
+#         f'[blue]{blue_ball:0>2d}[/blue]'
+#     )
 
-for i in range(n):
-    selected_balls = random.sample(red_balls, 6)
-    selected_balls.sort()
-    blue_ball = random.choice(blue_balls)
-    # 向表格中添加行（序号，红色球，蓝色球）
-    table.add_row(
-        str(i + 1),
-        f'[red]{" ".join([f"{ball:0>2d}" for ball in selected_balls])}[/red]',
-        f'[blue]{blue_ball:0>2d}[/blue]'
-    )
-
-# 通过控制台输出表格
-console.print(table)
+# console.print(table)
     
+
