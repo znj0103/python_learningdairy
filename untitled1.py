bd3a0038b0c71634a31ac8294ccbfd71ce5baba7
znj0103 <znj0103@163.com>
2025-12-11 17:50:28 +0800

    aa

--- untitled1.py
+++ untitled1.py
@@ -286,5 +286,38 @@ Created on Tue Mar 25 13:24:46 2025
 #列表的应用
 #例4 双色球
 import random
-balls = [[random.randrange(1,34) for _ in range(6)],[random.randrange(1,17)]]
-print(balls)
+n = int(input('几注：'))
+red_balls = [i for i in range(1,34)]
+blue_balls = [i for i in range(1,17)]
+ball = []
+
+
+from rich.console import Console
+from rich.table import Table
+
+# 创建控制台
+console = Console()
+
+n = int(input('生成几注号码: '))
+red_balls = [i for i in range(1, 34)]
+blue_balls = [i for i in range(1, 17)]
+
+# 创建表格并添加表头
+table = Table(show_header=True)
+for col_name in ('序号', '红球', '蓝球'):
+    table.add_column(col_name, justify='center')
+
+for i in range(n):
+    selected_balls = random.sample(red_balls, 6)
+    selected_balls.sort()
+    blue_ball = random.choice(blue_balls)
+    # 向表格中添加行（序号，红色球，蓝色球）
+    table.add_row(
+        str(i + 1),
+        f'[red]{" ".join([f"{ball:0>2d}" for ball in selected_balls])}[/red]',
+        f'[blue]{blue_ball:0>2d}[/blue]'
+    )
+
+# 通过控制台输出表格
+console.print(table)
+    
